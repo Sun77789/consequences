e4af62454ee44d15aa164d1cf87295b497698ebb
Malika Aubakirova <aubakirova.m.m@gmail.com>
2014-04-05 16:48:50 -0500

    Added login page

---
+++ consequences/LoginViewController.h
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.h
+//  Crushes
+//
+//  Created by Malika Aubakirova on 3/26/14.
+//  Copyright (c) 2014 Malika Aubakirova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+#import "customizedLoginViewController.h"
+
+@interface LoginViewController : UIViewController <PFLogInViewControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UILabel *welcomeLabel;
+@property (strong, nonatomic) NSString *chosenUni;
+
+@end
