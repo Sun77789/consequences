2807c5363814a98e373d13201bc80d0426c3f5f9
Geoffroy Lesage <jeff@Geoffroys-MacBook-Pro.local>
2014-04-05 15:22:38 -0500

    Initial app created

---
+++ consequences/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  consequences
+//
+//  Created by Geoffroy Lesage on 4/5/14.
+//  Copyright (c) 2014 Geoffroy Lesage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
